d59ffabdc801975bf1c0f8c884d59b6d97b121e0
Kaveh Doroudi <doroudi.kaveh@gmail.com>
2016-02-24 22:24:37 -0800

    more stuff

---
+++ monkAndEulerCodes/InterviewQuestion/05_morse_code.rb
@@ -0,0 +1,49 @@
+def morse_encode(str)
+  sentence = str.split(" ")
+  mor_sent = ""
+  for i in 0..(sentence.length - 1)
+    mor_sent << (morse_encode_word(sentence[i]) + " ")
+  end
+  mor_sent
+end
+
+def morse_encode_word(word)
+  morse_encode_char =
+  {
+    "A"	=> ".- " ,
+    "B"	=> "-... " ,
+    "C"	=> "-.-. " ,
+    "D"	=> "-.. " ,
+    "E"	=> ". " ,
+    "F"	=> "..-. " ,
+    "G"	=> "--. " ,
+    "H"	=> ".... " ,
+    "I"	=> ".. " ,
+    "J"	=> ".--- " ,
+    "K"	=> "-.- " ,
+    "L"	=> ".-.. " ,
+    "M"	=> "-- " ,
+    "N"	=> "-. " ,
+    "O"	=>	"--- " ,
+    "P"	=>	".--. " ,
+    "Q"	=>	"--.- " ,
+    "R"	=>	".-. " ,
+    "S"	=>	"... " ,
+    "T"	=>	"- " ,
+    "U"	=>	"..- " ,
+    "V"	=>	"...- " ,
+    "W"	=>	".-- " ,
+    "X"	=>	"-..- " ,
+    "Y"	=>	"-.-- " ,
+    "Z"	=>	"--.. "
+  }
+  m_word = ""
+  translate = word.upcase
+  for i in 0..(translate.length - 1)
+    m_word << morse_encode_char["#{translate[i]}"]
+  end
+  m_word
+end
+
+
+puts morse_encode "my name is kaveh"
